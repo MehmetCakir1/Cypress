2853ad6de52ba8ec47d6f1c24fd18f70448763ab
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-10-28 13:09:39 +0300

    navbar test

--- cypress/component/Navbar.cy.jsx
+++ cypress/component/Navbar.cy.jsx
@@ -1,4 +1,3 @@
-import cypress from "cypress"
 import Navbar from "../../src/components/Navbar"
 import React from "react"
 
@@ -10,10 +9,37 @@ describe("Navbar Test",()=>{
         )
     })
     it("Navbar Test",()=>{
-        cy.get("nav")
+        cy.get("a")
     })
 
-
+    it("First Link Test",()=>{
+        const firstLink="[data-cy=first]"
+        cy.get(firstLink).should("have.text","HOME")
+    })
+    it("Testing if the number of links equals to 4",()=>{
+        cy.get("[aria-label=link]").should("have.length",4)
+        cy.get(".linkDiv a").should("have.length",4)
+    })
+    it("Testing if the links have 'hover:text-red-500' classname ",()=>{
+        cy.get(".linkDiv a").should("have.class","hover:text-red-500")
+    })
+    it("Testing if last link contains 'SERVICES' ",()=>{
+        cy.get(".linkDiv a").last()
+        .should("have.text","SERVICES")
+    })
+    it("Testing if 'CONTACT' link has a class named 'third' ",()=>{
+        cy.get(".linkDiv a")
+        .contains(/contact/i)
+        .should("have.class","third")
+    })
+    it("Testing if the number of links without 'temp' classname equals to 2 ",()=>{
+        cy.get(".linkDiv a:not('.temp')")
+        .should("have.length",2)
+        .within(() => {
+            cy.root().eq(0).should('have.text', "ABOUT")
+            cy.root().eq(1).should('have.text', "SERVICES")
+          })
+    })
 
 
 })
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav>
         <div className='flex justify-between items-center px-5'>
             <img src="images/logo.png" alt="logo" className='w-[70px]'/>
-            <div className='font-bold flex gap-x-5'>
-              <a href="#" className='hover:text-red-500'>HOME</a>
-              <a href="#" className='hover:text-red-500'>ABOUT</a>
-              <a href="#" className='hover:text-red-500'>CONTACT</a>
-              <a href="#" className='hover:text-red-500'>SERVICES</a>
+            <div className='linkDiv font-bold flex gap-x-5'>
+              <a href="#" className='hover:text-red-500 link first temp' data-cy="first" aria-label="link">HOME</a>
+              <a href="#" className='hover:text-red-500 link second' aria-label="link">ABOUT</a>
+              <a href="#" className='hover:text-red-500 link third temp' aria-label="link" >CONTACT</a>
+              <a href="#" className='hover:text-red-500 link fourth' aria-label="link">SERVICES</a>
             </div>
         </div>
     </nav>
